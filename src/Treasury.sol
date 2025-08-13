0d17b5483561a203dec725fff99ef89ecddb1d2d
Tranquil Flow <tranquil_flow@protonmail.com>
2025-08-13 21:13:52 +0200

    forge: implement initial skeleton of contracts based on tech spec

---
+++ src/Treasury.sol
@@ -0,0 +1,169 @@
+pragma solidity 0.8.30;
+
+// Consolidates all protocol-wide USDC flows, profit/loss accounting, and insurance buffer management within a single orchestrator contract.
+// Profit & Loss Reporter: Records epoch-based performance and triggers P&L adjustments.
+// Insurance Buffer: Maintains a reserve to cover potential Asset Manager losses.
+// Asset Manager Allocator: Routes USDC deposits/withdrawals to the Asset Manager.
+
+// Receives USDC from deposits to the USX contract
+
+// Only this contract can mint/burn USX
+
+// USDC is transferred to/from Asset Manager contract
+
+// Upgradeable smart contract UUPS
+// ERC7201
+
+contract Treasury {
+
+    /*=========================== State Variables =========================*/
+
+    /*=========================== Public Functions =========================*/
+
+    /*=========================== Governance Functions =========================*/
+
+    /*=========================== Internal Functions =========================*/
+
+}
+
+contract ProfitAndLossReporter {
+
+    /*=========================== State Variables =========================*/
+
+    //  duration of epoch in blocks, (default == 216000 (30days))
+    uint256 public epochDuration;
+
+    // profits reported for previous period 
+    uint256 public netEpochProfits;
+
+    // current profit added at current epoch
+    uint256 public profitLatestEpoch;
+
+    // determines increase in profits for each block
+    uint256 public profitPerBlock;
+
+    address public governanceWarchest;
+
+
+    /*=========================== Public Functions =========================*/
+
+    // calculates the success fee for the Goverance Warchest based on successFeeFraction
+    function successFee() public view returns (uint256) {}
+
+    /*=========================== Governance Functions =========================*/
+
+    // duration of epoch in blocks, (default == 216000 (30days))
+    function setEpochDuration(uint256 _epochDurationBlocks) public onlyGovernance {}
+
+    // fraction of success fee determining the success fee, (default 5% == 50000) with precision to 0.001 percent
+    function setSuccessFeeFraction(uint256 _successFeeFraction) public onlyGovernance {}
+
+    /*=========================== Asset Manager Functions =========================*/
+
+    // reports profit or loss for the Asset Manager
+    // distributableYield = gross profit - max insurance accrual
+    // maxInsuranceAccrual = grossProfit * renewalFraction + netDeposits
+    // deal with profits reported and losses reported
+    // profits affect share price over linear period till next epoch
+    // losses immediately affect share price
+    function reportProfitAndLoss(int256 grossValueChange) public onlyAssetManager {
+        // profits
+        _distributeProfits();
+
+        // losses
+        _distributeLosses();
+    }
+
+    /*=========================== Internal Functions =========================*/
+
+    // updates peg, by taking all outstanding USDC in the system (treasury & asset manager holdings) and dividing them by total supply of USX. 
+    // USDCoutstanding / USXtotalSupply
+    function _updatePeg() internal {}
+
+    function _distributeProfits() internal {}
+    // part of profits sent to Insurance Buffer
+    // part of profits sent to Governance Warchest
+
+    function _distributeLosses() internal {}
+}
+
+contract InsuranceBuffer {
+
+    /*=========================== State Variables =========================*/
+
+    // net deposits for the current epoch
+    uint256 public netDeposits;
+
+    // renewal fraction with precision to 0.001 percent (Minimum 10% == 100000)
+    uint32 public bufferRenewalRate;
+
+    // buffer target with precision to 0.001 percent (Minimum 5% == 50000)
+    uint32 public bufferTargetFraction;
+
+    /*=========================== Public Functions =========================*/
+
+    // returns current buffer target based on bufferTargetFraction and USX total supply
+    function bufferTarget() public view returns (uint256) {}
+
+    /*=========================== Governance Functions =========================*/
+
+    // sets renewal fraction with precision to 0.001 percent (Minimal value & default is 10% fee == 100000)
+    function setBufferRenewalRate(uint32 _bufferRenewalRate) public onlyGovernance {}
+
+    // sets buffer target with precision to 0.001 percent (Minimal value & default is 5% fee == 50000)
+    // can only be increased?
+    function setBufferTargetFraction(uint32 _bufferTargetFraction) public onlyGovernance {}
+
+    /*=========================== Internal Functions =========================*/
+
+    // tops up insurance buffer
+    // it is triggered within every reportProfitAndLoss() call reports positive rewards while insurance buffer is less then bufferTarget(). Tries to replenish buffer up to amount from first netDeposits from the latest epoch, then netEpochProfits
+    function topUpBuffer(uint256 _amount) internal {}
+
+    // deplete insurance buffer
+    // it is triggered within every reportProfitAndLoss() call reports a loss. Tries to drain buffer up to amount. If amount <= bufferSize, then it drains the buffer, if amount > bufferSize than the USX:USDC peg is broken to reflect the loss.
+    function _slashBuffer(uint256 _amount) internal {}
+}
+
+// receives USDC from USX contract deposits and routes to Asset Managers contract
+
+// facilitates withdrwal requests to Asset Manager, adhering to withdrawal period (15 days)
+
+// withdrawals subject to Asset Manager specific constraints and allocation limits
+
+// standaradized interface for Asset Manager's contract
+interface IAssetManager {
+    function deposit(uint256 _amount) external;
+    function withdraw(uint256 _amount) external;
+}
+
+contract AssetManagerAllocator {
+
+    /*=========================== State Variables =========================*/
+
+    // the current Asset Manager for the protocol
+    address public assetManager;
+
+    // (default == 10, max == 10)
+    uint256 public maxLeverage;
+
+    /*=========================== Public Functions =========================*/
+
+    function depositUSDC(uint256 _amount) public {}
+
+    /*=========================== Governance Functions =========================*/
+
+    // sets the current Asset Manager for the protocol
+    function setAssetManager(address _assetManager) public onlyGovernance {}
+
+    // sets the max leverage for the protocol
+    function setMaxLeverage(uint256 _maxLeverage) public onlyGovernance {}
+
+    /*=========================== Asset Manager Functions =========================*/
+
+    function transferUSDCtoAssetManager(uint256 _amount) public onlyAssetManager {}
+
+    function transferUSDCFromAssetManager(uint256 _amount) public onlyAssetManager {}
+
+    /*=========================== Internal Functions =========================*/
+}
---
+++ src/USX.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.8.30;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+    // Treasury contract can mint USX
+
+    // Admin role can whitelist users for minting/redeeming USX
+
+    // deposit USDC to get USX
+
+    // Governance (or Asset Manager?) will monitor for unfufilled withdrawal requests and request USDC from Asset Manager as needed
+
+    // Upgradeable smart contract UUPS
+    // ERC7201
+    // ReentrancyGuard
+
+contract USX is ERC20 {
+
+    /*=========================== State Variables =========================*/
+
+    address public USDC; // IERC20 import instead? immutable?
+
+    uint256 public outstandingWithdrawalAmount;
+
+    address public treasury;
+
+    address public governanceWarchest;
+
+    /*=========================== Public Functions =========================*/
+
+    function deposit() public {}
+    // check for qued withdrawals, fulfill withdrawals with new deposits
+    // outstanding net deposits are sent to the treasury contract
+
+    function requestUSDC() public {}
+    // check if withdrawals are frozen
+    // check if there is USDC on the treasury contract that haven't been sent to asset manager. If so user can call claimUSDC immediately.
+
+    function claimUSDC() public {}
+
+    function usxPrice() public view returns (uint256) {}
+
+    /*=========================== Internal Functions =========================*/
+
+}
